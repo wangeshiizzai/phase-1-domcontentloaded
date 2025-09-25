cd634b2011ae5f4e2fdc4e6949ae2930ef4242b2
wangeshiizzai <keshi.yvonne84@gmail.com>
2025-09-25 17:46:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementById("text");
+  element.textContent = "This is really cool!";
+});
